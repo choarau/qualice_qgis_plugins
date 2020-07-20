45e2d8d0cb2c02234eda96c7bb75d60e0265fa1d
MathildeSegaud <mathilde.segaud@ensg.eu>
2020-07-20 14:21:16 +0200

    nex version plugin V1 et un nouveau plugin V2

---
+++ creation_export_v2/test/test_resources.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+"""Resources test.
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+
+__author__ = 'mathilde.segaud@ensg.eu'
+__date__ = '2020-07-17'
+__copyright__ = 'Copyright 2020, Mathilde S'
+
+import unittest
+
+from qgis.PyQt.QtGui import QIcon
+
+
+
+class CreationExportV2DialogTest(unittest.TestCase):
+    """Test rerources work."""
+
+    def setUp(self):
+        """Runs before each test."""
+        pass
+
+    def tearDown(self):
+        """Runs after each test."""
+        pass
+
+    def test_icon_png(self):
+        """Test we can click OK."""
+        path = ':/plugins/CreationExportV2/icon.png'
+        icon = QIcon(path)
+        self.assertFalse(icon.isNull())
+
+if __name__ == "__main__":
+    suite = unittest.makeSuite(CreationExportV2ResourcesTest)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
+
+
+
